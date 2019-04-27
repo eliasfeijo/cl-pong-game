85033f4654d9d78741f9fac12e15016a27544395
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-04-27 01:32:26 -0300

    Fixing color values

--- util.lisp
+++ util.lisp
@@ -5,7 +5,7 @@
 (defvar *canvas-height* 600)
 
 (defvar *black* (vec4 0 0 0 1))
-(defvar *white* (vec4 255 255 255 1))
+(defvar *white* (vec4 1 1 1 1))
 
 
 
@@ -65,15 +65,15 @@
    (make-instance 'color
 		  :name "Red"
 		  :description "Burn your foe!"
-		  :value (vec4 255 0 0 1))
+		  :value (vec4 1 0 0 1))
    (make-instance 'color
 		  :name "Green"
 		  :description "Slow down your foe!"
-		  :value (vec4 0 255 0 1))
+		  :value (vec4 0 1 0 1))
    (make-instance 'color
 		  :name "Blue"
 		  :description "Increases the ball's speed."
-		  :value (vec4 0 0 255 1))))
+		  :value (vec4 0 0 1 1))))
 
 
 
